d7943304a7120b5dabdda15306b39de53c7d1dfb
k.mae <maetaro0614@gmail.com>
2019-03-06 07:04:45 +0900

    Update csv_parser.py

--- demo/csv_parser.py
+++ demo/csv_parser.py
@@ -15,30 +15,30 @@ class csv_parser:
     Example
     -------
     >>> p = csv_parser()
-    >>> p.parse("hi")
+    >>> p.parse("hi").result()
     "Left \"(unknown)\" (line 1, column 3):"
     "unexpected end of input"
     "expecting \",\" or \"\\n""
-    >>> p.parse("hi\\n")
-    [["hi"]]
-    >>> p.parse("line1\\nline2\\nline3\\n")
-    [["line1"],["line2"],["line3"]]
-    >>> p.parse("cell1,cell2,cell3\\n")
-    [["cell1","cell2","cell3"]]
-    >>> p.parse("l1c1,l1c2\\nl2c1,l2c2\\n")
-    [["l1c1","l1c2"],["l2c1","l2c2"]]
+    >>> p.parse('"hi"\\n').result()
+    [['"hi"']]
+    >>> p.parse('"line1"\\n"line2"\\n"line3"\\n').result()
+    [['"line1"'],['"line2"'],['"line3"']]
+    >>> p.parse('"cell1","cell2","cell3"\\n').result()
+    [['"cell1"', '"cell2"', '"cell3"']]
+    >>> p.parse('"l1c1","l1c2"\\n"l2c1","l2c2"\\n').result()
+    [['"l1c1"', '"l1c2"'], ['"l2c1"', '"l2c2"']]
     '''
     def parse(self, s):
 
         dquote = p.token("\"")
-        #quotedChar = p.choice(p.noneOf(dquote), p.option("\"\""))
-        #quotedCell = p.seq(p.token("("), quotedChar, p.token(")"))
-        cell = p.choice(quotedCell, p.many(p.noneOf(",\n\r")))
-        line = p.seq(cell, p.option(p.char(',')))
-        #eol = p.choice(p.token("\n\r"), p.token("\r\n"), p.token("\n"), p.token("\r"))
+        cell = p.map(p.seq(dquote, p.regex("\w*"), dquote), lambda x: "".join(x))
+        line = p.map(p.sepBy(cell, p.char(',')), lambda x: [x])
+        newLine = p.choice(p.token("\n\r"), p.token("\r\n"), p.token("\n"), p.token("\r"))
+        lines = p.sepBy(cell, newLine)
+        eol = p.choice(p.token("\n\r"), p.token("\r\n"), p.token("\n"), p.token("\r"))
 
-        #parser = p.endBy(line, eol)
-        parser = line # p.sepBy(line, eol)
+        parser = p.endBy(line, eol)
+        #parser = line # p.sepBy(line, eol)
 
         #parseCSV :: String -> Either ParseError [[String]]
         #parseCSV input = parse csvFile "(unknown)" input

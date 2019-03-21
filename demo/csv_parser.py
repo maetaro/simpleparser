b07e9fdd8cedb882ddd139a73d586a5814f7ca21
k.mae <maetaro0614@gmail.com>
2019-03-22 05:58:31 +0900

    Update csv_parser.py

--- demo/csv_parser.py
+++ demo/csv_parser.py
@@ -11,51 +11,41 @@ import simple_parser as p
 def parseCsv(s):
     '''
     http://book.realworldhaskell.org/read/using-parsec.html
-
+    
     Example
     -------
     >>> p = parseCsv
-    >>> p.parse("hi").result()
+    >>> p("hi").result()
     "Left \"(unknown)\" (line 1, column 3):"
     "unexpected end of input"
     "expecting \",\" or \"\\n""
-    >>> p.parse('"hi"\\n').result()
+    >>> p('"hi"\\n').result()
     [['"hi"']]
-    >>> p.parse('"line1"\\n"line2"\\n"line3"\\n').result()
+    >>> p('"line1"\\n"line2"\\n"line3"\\n').result()
     [['"line1"'],['"line2"'],['"line3"']]
-    >>> p.parse('"cell1","cell2","cell3"\\n').result()
+    >>> p('"cell1","cell2","cell3"\\n').result()
     [['"cell1"', '"cell2"', '"cell3"']]
-    >>> p.parse('"l1c1","l1c2"\\n"l2c1","l2c2"\\n').result()
+    >>> p('"l1c1","l1c2"\\n"l2c1","l2c2"\\n').result()
     [['"l1c1"', '"l1c2"'], ['"l2c1"', '"l2c2"']]
+    >>> p("line1\\r\\nline2\\nline3\\n\\rline4\\rline5\\n").result()
+    [["line1"], ["line2"], ["line3"], ["line4"], ["line5"]]
     '''
-    def parse(self, s):
 
-        def line_selector(x):
-            print(x)
-            if len(x) == 0:
-                return None
-            if x is None:
-                return x
-            return [x]
+    def line_selector(x):
+        if len(x) == 0:
+            return None
+        if x is None:
+            return x
+        return [x]
 
-        dquote = p.token("\"")
-        cell = (dquote + p.regex("\w*") + dquote).map(lambda x: "".join(x))
-        line = p.sepBy(cell, p.char(',')).map(line_selector)
-        eol = p.token("\n\r") | p.token("\r\n") | p.token("\n") | p.token("\r")
+    dquote = p.token("\"")
+    cell = (dquote + p.regex("\w*") + dquote).map(lambda x: "".join(x))
+    line = p.sepBy(cell, p.char(',')).map(line_selector)
+    eol = p.token("\n\r") | p.token("\r\n") | p.token("\n") | p.token("\r")
 
-        parser = p.endBy(line, eol)
+    parser = p.endBy(line, eol)
 
-        #parseCSV :: String -> Either ParseError [[String]]
-        #parseCSV input = parse csvFile "(unknown)" input
-
-        #main =
-        #    do c <- getContents
-        #       case parse csvFile "(stdin)" c of
-        #            Left e -> do putStrLn "Error parsing input:"
-        #                         print e
-        #            Right r -> mapM_ print r
-
-        return parser.exec(s, 0)
+    return parser.exec(s, 0)
 
 
 if __name__ == "__main__":

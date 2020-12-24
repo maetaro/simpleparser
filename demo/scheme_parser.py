539dee9f45d7f37965d04f7a3450c659f5d9c2d3
maetaro <maetaro@users.noreply.github.com>
2020-12-25 07:36:35 +0900

    Update scheme_parser.py

--- demo/scheme_parser.py
+++ demo/scheme_parser.py
@@ -3,13 +3,14 @@
 import os
 import sys
 
-sys.path.append(os.path.join(os.path.dirname(__file__), '..\src'))
+sys.path.append(os.path.join(os.path.dirname(__file__), '../src'))
 
 import simple_parser as p
 
 
 class schemeparser:
-    """
+    """Schemeparser class.
+
     >>> parser = schemeparser()
     >>> parser.parse("( + 1 7 )").result()
     [True, ['(', '+', ' ', '1', ' ', '7', ')'], 7]
@@ -17,20 +18,22 @@ class schemeparser:
     [True, ['(', '+', ' ', '1', ' ', '(', ' ', '-', ' ', '2', ' ', '3', ')', ')'], 13]
     >>> parser.parse("( + 1 ( - 2 ( + 3 4 ) ) )").result()
     [True, ['(', '+', ' ', '1', ' ', '(', ' ', '-', ' ', '2', ' ', '3', ')', ')'], 13]
-    """
+    """  # noqa: E501
+
     def parse(self, s):
+        """Parse method."""
         l_paren = p.token("(")
         r_paren = p.token(")")
-        symbol = p.regex("\S")
-        blank = p.regex("\s")
-        opt_blank = p.option(blank)
+        symbol = p.regex(r"\S")
+        blank = p.regex(r"\s")
+        # opt_blank = p.option(blank)
 
-        #operator = symbol
-        #num = symbol
-        #parenthesis = p.lazy(lambda: p.seq(l_paren, expression, r_paren))
-        #atom = p.choice(num, parenthesis)
-        #expression = p.seq(atom, p.many(p.seq(operator, atom)))
-        #parser = expression
+        # operator = symbol
+        # num = symbol
+        # parenthesis = p.lazy(lambda: p.seq(l_paren, expression, r_paren))
+        # atom = p.choice(num, parenthesis)
+        # expression = p.seq(atom, p.many(p.seq(operator, atom)))
+        # parser = expression
 
         exp = p.lazy(lambda:
                      p.seq(l_paren,
@@ -43,7 +46,7 @@ class schemeparser:
                            r_paren)
                      )
         parser = exp
-        return parser(s, 0)
+        return parser.exec(s, 0)
 
 
 if __name__ == "__main__":

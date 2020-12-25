890e0caa83a44445cb28628af874d906fc5ff361
maetaro <maetaro@users.noreply.github.com>
2020-12-25 16:05:28 +0900

    Update test_parser.py

--- test_parser.py
+++ test_parser.py
@@ -1,8 +1,14 @@
 """test."""
 
+from simple_parser.simple_parser import char, token, Success
 
 def test_1() -> None:
     """test_1."""
     a: int = 1
     b: int = 1
     assert a == b
+
+
+def test_token_1() -> None:
+    f = char("foo")
+    assert f.exec("foobar").result() == ["foo"]

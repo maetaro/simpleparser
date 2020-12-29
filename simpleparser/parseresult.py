bc4c8d398824c14b5094a8f49d0e87e0c5da4744
maetaro <maetaro@users.noreply.github.com>
2020-12-29 17:55:55 +0900

    Update parseresult.py

--- simpleparser/parseresult.py
+++ simpleparser/parseresult.py
@@ -1,6 +1,6 @@
 """a simple parser combinator."""
 
-from typing import List
+from typing import List, Callable
 
 
 class ParseResult:
@@ -14,41 +14,53 @@ class ParseResult:
         self.position: int = position
         self.message: str = message
 
-    def then(self, f):
+
+class Success(ParseResult):
+    """Parsed Success class."""
+
+    def __init__(self, tokens: List[str], position: int) -> None:
+        """Initialize method."""
+        super().__init__(True, tokens, position)
+
+    def __repr__(self) -> str:
+        """Return string."""
+        return str(self.tokens)
+
+    def then(self, f: Callable[[ParseResult], None]) -> ParseResult:
         """Execute function then parse is success."""
         if self.success:
             f(self)
         return self
 
-    def catch(self, f):
+    def catch(self, f: Callable[[ParseResult], None]) -> ParseResult:
         """Execute function then parse is success."""
         if not self.success:
             f(self)
         return self
 
 
-class Success(ParseResult):
-    """Parsed Success class."""
-
-    def __init__(self, tokens: List[str], position: int):
-        """Initialize method."""
-        super().__init__(True, tokens, position)
-
-    def __repr__(self):
-        """Return string."""
-        return str(self.tokens)
-
-
 class Failure(ParseResult):
     """Parsed Failure class."""
 
-    def __init__(self, message: str, position: int):
+    def __init__(self, message: str, position: int) -> None:
         """Initialize method."""
         super().__init__(False, [], position, message)
 
-    def __repr__(self):
+    def __repr__(self) -> str:
         """Return string."""
         return self.message
+
+    def then(self, f: Callable[[ParseResult], None]) -> ParseResult:
+        """Execute function then parse is success."""
+        if self.success:
+            f(self)
+        return self
+
+    def catch(self, f: Callable[[ParseResult], None]) -> ParseResult:
+        """Execute function then parse is success."""
+        if not self.success:
+            f(self)
+        return self
 
 
 if __name__ == "__main__":

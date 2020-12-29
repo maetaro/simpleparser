f0cae5c2278aec7290c14be2849eda59af5acaea
maetaro <maetaro@users.noreply.github.com>
2020-12-29 21:37:10 +0900

    Update demo_json_parser.py

--- demo_json_parser.py
+++ demo_json_parser.py
@@ -1,6 +1,9 @@
 """csv parsing sample."""
 
-from simpleparser import token, regex, choice, seq, lazy, sepBy, option, map
+from simpleparser import (
+    token, regex, choice, seq, lazy, sepBy, option, map,
+    ParseResult, Parser
+)
 
 
 class json_parser:
@@ -12,30 +15,31 @@ class json_parser:
     >>> p.parse("{}")
     ['{', '}']
     >>> p.parse("{p1:1,p2:2}")
-    ['{', 'p1', ':', 1, 'p2', ':', 2, '}']
+    ['{', 'p1', ':', '1', 'p2', ':', '2', '}']
     >>> p.parse("{p1:1,p2:{p1:1,p2:2}}")
-    ['{', 'p1', ':', 1, 'p2', ':', '{', 'p1', ':', 1, 'p2', ':', 2, '}', '}']
+    ['{', 'p1', ':', '1', 'p2', ':', '{', 'p1', ':', '1', 'p2', ':', '2', '}', '}']  # noqa F501
     >>> p.parse("[[1,2,3],[2],[3]]")
-    ['[', '[', 1, 2, 3, ']', '[', 2, ']', '[', 3, ']', ']']
+    ['[', '[', '1', '2', '3', ']', '[', '2', ']', '[', '3', ']', ']']
     >>> p.parse("[1,2,3]")
-    ['[', 1, 2, 3, ']']
+    ['[', '1', '2', '3', ']']
     >>> p.parse("{p2:'a'}")
     ['{', 'p2', ':', "'a'", '}']
     >>> p.parse("{p1:1,p2:'a',p3:[]}")
-    ['{', 'p1', ':', 1, 'p2', ':', "'a'", 'p3', ':', '[', ']', '}']
+    ['{', 'p1', ':', '1', 'p2', ':', "'a'", 'p3', ':', '[', ']', '}']
     """
 
-    def parse(self, s):
+    def parse(self, s: str) -> ParseResult:
         """Parse method."""
-        propName = regex(r"\w+")
-        colon = token(":")
-        sq = token("'")
-        dq = token('"')
-        p_str = map(choice(seq(dq, regex(r"\w*"), dq), seq(sq, regex(r"\w*"), sq)), lambda x: "".join(x))  # noqa: E501
-        num = map(regex(r"\d+"), lambda x: int(float("".join(x))) if "".join(x) != "" else x)  # noqa: E501
-        p_multi = choice(num, p_str, lazy(lambda: obj), lazy(lambda: ary))
-        ary = seq(token("["), option(sepBy(p_multi, token(","))), token("]"))  # noqa: E501
-        obj = seq(token("{"), option(sepBy(seq(propName, colon, p_multi), token(","))), token("}"))  # noqa: E501
+        propName: Parser = regex(r"\w+")
+        colon: Parser = token(":")
+        sq: Parser = token("'")
+        dq: Parser = token('"')
+        p_str: Parser = map(choice(seq(dq, regex(r"\w*"), dq), seq(sq, regex(r"\w*"), sq)), lambda x: ["".join(x)])  # noqa: E501
+        num: Parser = map(regex(r"\d+"), lambda x: [str(int(float("".join(x))))] if "".join(x) != "" else x)  # noqa: E501
+        p_multi: Parser = choice(num, p_str, lazy(
+            lambda: obj), lazy(lambda: ary))
+        ary: Parser = seq(token("["), option(sepBy(p_multi, token(","))), token("]"))  # noqa: E501
+        obj: Parser = seq(token("{"), option(sepBy(seq(propName, colon, p_multi), token(","))), token("}"))  # noqa: E501
 
         if s.lstrip()[0] == '[':
             return ary.exec(s, 0)

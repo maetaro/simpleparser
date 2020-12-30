e687a86358760debcc790d32ffc75f5bd057dc0e
maetaro <maetaro@users.noreply.github.com>
2020-12-30 21:47:57 +0900

    Update comb.py

--- simpleparser/comb.py
+++ simpleparser/comb.py
@@ -215,6 +215,96 @@ def transform(parser: Parser, selector: Callable[[List[str]], List[str]]) -> Par
     return Parser(f)
 
 
+def endBy(parser: Parser, sep: Parser) -> Parser:
+    r"""Endby p sep parses zero or more occurrences of p, separated and ended by sep.
+
+    Returns a list of values returned by p.
+
+    Example
+    -------
+    >>> from simpleparser import token, regex, endBy
+    >>> endBy(regex('\w*'), token(',')).exec('')
+    ['']
+    >>> endBy(regex('\w*'), token(',')).exec('hoge,hoge')
+    ['hoge', 'hoge']
+    >>> endBy(regex('\w*'), token(',')).exec('hoge,hoge,')
+    ['hoge', 'hoge', '']
+    >>> endBy(regex('\w*'), token(',')).exec('hoge,hoge,-')
+    parse error.
+    """  # noqa: D401, E501
+    def f(target: str, position: int = 0) -> ParseResult:
+        result = []
+        pos: int = position
+
+        while True:
+            parsed = parser.exec(target, pos)
+            if not parsed.success:
+                break
+            result.extend(parsed.tokens)
+            pos = parsed.position
+
+            parsed = sep.exec(target, pos)
+            if not parsed.success:
+                break
+            pos = parsed.position
+
+        if pos != len(target):
+            return Failure("parse error.", pos)
+
+        return Success(result, pos)
+
+    return Parser(f)
+
+
+def sepBy(parser: Parser, sep: Parser) -> Parser:
+    r"""Parse zero or more occurrences of parser, separated by sep. Returns a list of values returned by parser.
+
+    Example
+    -------
+    >>> from simpleparser import token, regex, sepBy
+    >>> sepBy(regex('\w*'), token(',')).exec('hoge,hoge')
+    ['hoge', 'hoge']
+    """  # noqa: D401, E501
+    def f(target: str, position: int = 0) -> ParseResult:
+        result = []
+        pos = position
+
+        while True:
+            parsed = parser.exec(target, pos)
+            if not parsed.success:
+                break
+            result.extend(parsed.tokens)
+            pos = parsed.position
+
+            parsed = sep.exec(target, pos)
+            if not parsed.success:
+                break
+            pos = parsed.position
+
+        return Success(result, pos)
+
+    return Parser(f)
+
+
+def lazy(callback: Callable[[], Parser]) -> Parser:
+    """Lazy function.
+
+    Example
+    -------
+    >>> from simpleparser import token, seq, option, lazy
+    >>> p = option(seq(token('foo'), lazy(lambda: p)))
+    >>> p.exec('foo')
+    ['foo']
+    >>> p.exec('foofoo')
+    ['foo', 'foo']
+    """
+    def f(target: str, position: int) -> ParseResult:
+        parse = callback()
+        return parse.exec(target, position)
+
+    return Parser(f)
+
+
 if __name__ == "__main__":
     import doctest
     doctest.testmod()

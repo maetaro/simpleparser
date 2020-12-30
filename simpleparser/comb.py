e7f8c21ddbd7cbfde56108f8f43a51a69a9aac78
maetaro <maetaro@users.noreply.github.com>
2020-12-30 16:17:17 +0900

    Update comb.py

--- simpleparser/comb.py
+++ simpleparser/comb.py
@@ -1,6 +1,6 @@
 """a simple parser combinator."""
 
-from typing import List
+from typing import List, Callable
 from simpleparser.parseresult import ParseResult, Success, Failure
 from simpleparser.parser import Parser
 

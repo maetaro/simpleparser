7c4cc99bf4cdaf7e85eb2038410b7d19fdcf0900
maetaro <maetaro@users.noreply.github.com>
2020-12-31 16:39:21 +0900

    Update demo_json_parser.py

--- demo/demo_json_parser.py
+++ demo/demo_json_parser.py
@@ -2,16 +2,16 @@
 
 from simpleparser import (
     token, regex, choice, seq, lazy, sep_by, option, transform,
-    ParseResult, Parser
+    ParseResult
 )
 
 
-class json_parser:
+class JsonParser:
     """Json parser.
 
     Example
     -------
-    >>> p = json_parser()
+    >>> p = JsonParser()
     >>> p.parse("{}")
     ['{', '}']
     >>> p.parse("{p1:1,p2:2}")
@@ -30,15 +30,15 @@ class json_parser:
 
     def parse(self, s: str) -> ParseResult:
         """Parse method."""
-        propName: Parser = regex(r"\w+")
-        colon: Parser = token(":")
-        sq: Parser = token("'")
-        dq: Parser = token('"')
-        p_str: Parser = transform(choice(seq(dq, regex(r"\w*"), dq), seq(sq, regex(r"\w*"), sq)), lambda x: ["".join(x)])  # noqa: E501
-        num: Parser = transform(regex(r"\d+"), lambda x: [str(int(float("".join(x))))] if "".join(x) != "" else x)  # noqa: E501
-        p_multi: Parser = choice(num, p_str, lazy(lambda: obj), lazy(lambda: ary))
-        ary: Parser = seq(token("["), option(sep_by(p_multi, token(","))), token("]"))  # noqa: E501
-        obj: Parser = seq(token("{"), option(sep_by(seq(propName, colon, p_multi), token(","))), token("}"))  # noqa: E501
+        propName = regex(r"\w+")
+        colon = token(":")
+        sq = token("'")
+        dq = token('"')
+        p_str = transform(choice(seq(dq, regex(r"\w*"), dq), seq(sq, regex(r"\w*"), sq)), lambda x: ["".join(x)])  # noqa: E501
+        num = transform(regex(r"\d+"), lambda x: [str(int(float("".join(x))))] if "".join(x) != "" else x)  # noqa: E501
+        p_multi = choice(num, p_str, lazy(lambda: obj), lazy(lambda: ary))
+        ary = seq(token("["), option(sep_by(p_multi, token(","))), token("]"))  # noqa: E501
+        obj = seq(token("{"), option(sep_by(seq(propName, colon, p_multi), token(","))), token("}"))  # noqa: E501
 
         if s.lstrip()[0] == '[':
             return ary.exec(s, 0)

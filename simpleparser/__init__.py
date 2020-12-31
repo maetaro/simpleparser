a0752d1881f7e3727ffad190fac96b31f47ab409
maetaro <maetaro@users.noreply.github.com>
2020-12-31 22:41:26 +0900

    Update __init__.py

--- simpleparser/__init__.py
+++ simpleparser/__init__.py
@@ -2,6 +2,6 @@
 
 from simpleparser.parseresult import ParseResult, Success, Failure  # noqa F401
 from simpleparser.parser import Parser  # noqa F401
-from simpleparser.prim import token, regex  # noqa F401
+from simpleparser.prim import token, regex, none_of  # noqa F401
 from simpleparser.comb import many, choice, seq, option, transform, sep_by, end_by, lazy  # noqa F401
 from simpleparser import builtin_parsers  # noqa F401

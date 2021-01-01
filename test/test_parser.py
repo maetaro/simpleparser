cc5157d01a53fc91d9cf17c276d448cda511b5ee
maetaro <maetaro@users.noreply.github.com>
2021-01-01 17:08:04 +0900

    Update test_parser.py

--- test/test_parser.py
+++ test/test_parser.py
@@ -34,3 +34,12 @@ def test_regex_1() -> None:
     """test_regex_2."""
     f = regex("([1-9]+)")
     assert f.exec("123").tokens == ["123"]
+
+
+def test_none_of_1() -> None:
+    from simpleparser import none_of, choice, token, many, transform
+    quotedChar = choice(token('""'), none_of('"'))
+    p = transform(many(quotedChar), lambda x: ["".join(x)])
+    text = r'Shirt with ""Haskell"" text"'
+    assert p.exec(text).tokens == ['Shirt with ""Haskell"" text']
+

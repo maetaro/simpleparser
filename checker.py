ff2e542610e6fb68fe6e0a5c3b13a8ebb442e9d5
maetaro <maetaro@users.noreply.github.com>
2021-01-03 12:51:24 +0900

    Update checker.py

--- checker.py
+++ checker.py
@@ -7,8 +7,8 @@ from typing import List
 def run() -> None:
     """Run check utils."""
     cmds: List[str] = [
-        "flake8 simpleparser --count --select=E9,F63,F7,F82 --show-source --statistics",  # noqa E501
-        "flake8 simpleparser --count --max-complexity=10 --max-line-length=127 --statistics",  # noqa E501
+        "flake8 simpleparser test demo --count --select=E9,F63,F7,F82 --show-source --statistics",  # noqa E501
+        "flake8 simpleparser test demo --count --max-complexity=10 --max-line-length=127 --statistics",  # noqa E501
         "mypy .",
         "pytest",
         "coverage xml",

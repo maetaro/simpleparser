1c4cf53e49761abfdce5863dc9a978fec2aef81a
maetaro <maetaro@users.noreply.github.com>
2021-01-03 19:37:30 +0900

    Update conf.py

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -35,7 +35,6 @@ extensions = [
     'sphinx.ext.napoleon',
     'sphinx.ext.autosummary',
     'sphinx.ext.todo',
-    'sphinx.ext.viewcode'
 ]
 
 # Add any paths that contain templates here, relative to this directory.
@@ -57,7 +56,7 @@ html_theme = 'basicstrap'
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ['_static']
+html_static_path = []
 
 # Include Python objects as they appear in source files
 # Default: alphabetically ('alphabetical')

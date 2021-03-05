aa223a1bd97f8f9e5390e139d073298f27135309
maetaro <maetaro0614@gmail.com>
2021-03-05 19:41:37 +0900

    add test ParseResult.then and ParseResult.catch

---
+++ test/test_parseresult.py
@@ -0,0 +1,25 @@
+"""test of ParseResult."""
+
+from simpleparser import token, Parser, Failure, Success
+
+
+def test_then_1() -> None:
+    """test_then_1."""
+    f: Parser = token("foo")
+
+    def f_then(result: Success):
+        assert result.tokens == ["foo"]
+    def f_catch(result: Failure):
+        assert False, "成功した場合は空振りする必要がある。"
+    assert f.exec("foobar").then(f_then).catch(f_catch)
+
+
+def test_catch_1() -> None:
+    """test_catch_1."""
+    f: Parser = token("bar")
+
+    def f_then(result: Success):
+        assert False, "失敗した場合は空振りする必要がある。"
+    def f_catch(result: Failure):
+        assert result.tokens == []
+    assert f.exec("foobar").then(f_then).catch(f_catch)

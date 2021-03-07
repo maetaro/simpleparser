a8b0205532ac74bcca75bd721317d9cc0ba9e05f
maetaro <maetaro0614@gmail.com>
2021-03-07 15:52:22 +0900

    Add type annotation.

--- test/test_parseresult.py
+++ test/test_parseresult.py
@@ -1,5 +1,6 @@
 """test of ParseResult."""
 
+from simpleparser.parseresult import ParseResult
 from simpleparser import token, Parser, Failure, Success
 
 
@@ -7,10 +8,10 @@ def test_then_1() -> None:
     """test_then_1."""
     f: Parser = token("foo")
 
-    def f_then(result: Success):
+    def f_then(result: ParseResult) -> None:
         assert result.tokens == ["foo"]
 
-    def f_catch(result: Failure):
+    def f_catch(result: ParseResult) -> None:
         assert False, "成功した場合は空振りする必要がある。"
 
     assert f.exec("foobar").then(f_then).catch(f_catch)
@@ -20,10 +21,10 @@ def test_catch_1() -> None:
     """test_catch_1."""
     f: Parser = token("bar")
 
-    def f_then(result: Success):
+    def f_then(result: ParseResult) -> None:
         assert False, "失敗した場合は空振りする必要がある。"
 
-    def f_catch(result: Failure):
+    def f_catch(result: ParseResult) -> None:
         assert result.tokens == []
 
     assert f.exec("foobar").then(f_then).catch(f_catch)
